0e92e7a57b3035fbc435ee23c31cde3e8a24e602
Vftdan <vftdan@gmail.com>
2025-06-15 19:19:29 +0200

    Implement spin-lock

--- objects.mk
+++ objects.mk
@@ -12,6 +12,7 @@ csr_h = $(SRC_DIR)/csr.h $(SRC_DIR)/csrs.cc
 exchandlers_h = $(SRC_DIR)/exchandlers.h
 base_types_h = $(SRC_DIR)/base_types.h $(SRC_DIR)/registers.cc $(csr_h)
 vmem_h = $(SRC_DIR)/vmem.h $(base_types_h)
+sync_h = $(SRC_DIR)/sync.h
 
 $(BUILD_DIR)/hypervisor.o: $(SRC_DIR)/hypervisor.c $(hypervisor_h) $(csr_h) $(exchandlers_h)
 	mkdir -p $(shell dirname "$@")
---
+++ src/sync.h
@@ -0,0 +1,33 @@
+#ifndef SRC_SYNC_H_
+#define SRC_SYNC_H_
+
+#include <stdatomic.h>
+#include <stdbool.h>
+
+typedef struct {
+	volatile atomic_flag flag;
+} SpinLock;
+
+#define SPIN_LOCK_UNLOCKED (SpinLock) { .flag = ATOMIC_FLAG_INIT }
+
+// Returns true if the acquiry was successful
+__attribute__((unused)) __attribute__((warn_unused_result))
+inline static bool spin_lock_try_acquire(SpinLock *lock)
+{
+	return !atomic_flag_test_and_set(&lock->flag);
+}
+
+__attribute__((unused))
+inline static void spin_lock_acquire(SpinLock *lock)
+{
+	while (atomic_flag_test_and_set(&lock->flag))
+		;  // Busy wait
+}
+
+__attribute__((unused))
+inline static void spin_lock_release(SpinLock *lock)
+{
+	atomic_flag_clear(&lock->flag);
+}
+
+#endif /* end of include guard: SRC_SYNC_H_ */
