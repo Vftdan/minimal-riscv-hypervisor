31420704a10beb7465f036ca519f9e7514d1bd65
Vftdan <vftdan@gmail.com>
2025-07-14 16:21:18 +0200

    Detect stack trace loops

--- src/panic.c
+++ src/panic.c
@@ -12,6 +12,8 @@ void panic(void)
 	HostThreadData *ctx = get_host_thread_address();
 	if (ctx) {
 		StackTraceEntry *entry = ctx->stack_trace_caller;
+		StackTraceEntry *entry_slow = entry;
+		bool update_slow = false;
 		while (entry) {
 			print_string("\tat ");
 			if (entry->description) {
@@ -19,8 +21,22 @@ void panic(void)
 			} else {
 				print_string("(null)");
 			}
-			print_string("\n");
+			print_string(" (stacktrace entry is at ");
+			print_addr((uintptr_t) entry);
+			print_string(")\n");
+			if (entry_slow == entry->caller) {
+				print_string("\tStacktrace cycle to ");
+				print_addr((uintptr_t) entry_slow);
+				print_string("!\n");
+				break;
+			}
 			entry = entry->caller;
+			if (update_slow) {
+				entry_slow = entry_slow->caller;
+				update_slow = false;
+			} else {
+				update_slow = true;
+			}
 		}
 	}
 #endif
