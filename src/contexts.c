f015dc0b978b7712a02ad8819ebe911e8b177c01
Vftdan <vftdan@gmail.com>
2025-08-16 16:26:25 +0200

    Initialize emulated `mstatus.MPIE` to 1

--- src/contexts.c
+++ src/contexts.c
@@ -1,6 +1,6 @@
 #include "contexts.h"
 
-#define GUEST_THREAD_CONTEXT_INIT (GuestThreadContext) {.csr.mstatus_mdt = true, .privelege_level = PL_MACHINE}
+#define GUEST_THREAD_CONTEXT_INIT (GuestThreadContext) {.csr.mstatus_mdt = true, .csr.mstatus_mpie = true, .privelege_level = PL_MACHINE}
 #define GUEST_MACHINE_DATA_INIT (GuestMachineData) {.uart.input_buffer = BYTE_RING_BUFFER_INIT_LIT, .uart.output_buffer = BYTE_RING_BUFFER_INIT_LIT}
 
 GuestThreadContext guest_threads[MAX_GUESTS][MAX_VIRT_HARTS] = {{GUEST_THREAD_CONTEXT_INIT}};
