eb2c261cb497a0c4b6c1fbae9c0154abe08fa617
Vftdan <vftdan@gmail.com>
2025-11-27 16:24:21 +0100

    Add xv6 submodule, use it when running the hypervisor

--- local.mk
+++ local.mk
@@ -2,3 +2,16 @@
 # Add GUEST_MEMORY_OFFSET (defined in src/vmem.h) to the addresses
 # Example:
 # QEMULOAD=-device loader,file=example/kernel.bin,addr=0x85000000
+
+QEMULOAD=\
+	 -device loader,file=xv6/kernel/kernel.bin,addr=0x85000000 \
+	 -device loader,file=xv6/fs.img,addr=0xc5000000
+run: xv6/kernel/kernel.bin xv6/fs.img
+xv6/kernel/kernel.bin: xv6/kernel/kernel
+	$(OBJCOPY) -O binary $< $@
+xv6/kernel/kernel: xv6/Makefile
+	cd xv6 && $(MAKE) kernel/kernel
+xv6/fs.img: xv6/Makefile
+	cd xv6 && $(MAKE) fs.img
+xv6/Makefile:
+	git submodule update --init --recursive
